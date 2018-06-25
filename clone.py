7d309ec41c996778a61b581cb1258508492a7bd8
jeongwhanchoi <jeongwhan.choi@icloud.com>
2018-06-25 10:46:28 +0000

    data augmentation and multiple cameras

--- clone.py
+++ clone.py
@@ -11,16 +11,31 @@ with open('./data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = './data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = './data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+    correction = 0.1
     measurement = float(line[3])
+    # Center Camera
     measurements.append(measurement)
+    # Left Camera
+    measurements.append(measurement + correction)
+    # Right Camera
+    measurements.append(measurement - correction)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    # Flipping
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+    
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 
 from keras.models import Sequential
@@ -42,5 +57,5 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
-model.save('models/model.h5')
+model.save('models/augmented_model.h5')
 exit()
