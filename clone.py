9b5a63a3b83d1308099480b04e1c2bc1f5531b51
jeongwhanchoi <jeongwhan.choi@icloud.com>
2018-06-25 09:32:44 +0000

    add convolution layers

--- clone.py
+++ clone.py
@@ -25,10 +25,18 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
